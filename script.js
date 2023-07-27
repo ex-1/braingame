3f56a73b8b3a05a301ec9679ea20299ba46853c4
Михаил <120077694+ex-1@users.noreply.github.com>
2023-07-27 09:01:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -46,31 +46,30 @@ function refresh() {
 function process() {
 	let input_value = document.querySelector(".input-box input").value
 	if (input_value == "") {
-		console.log("process 1if")
 		updateData()
 		refresh()
 	} else if (input_value.length > 0) {
-		console.log("process 2if")
 		if (input_value == num) {
-			console.log("input = num", num, input_value)
 			lvl += 1
-			console.log("lvl up")
 			document.querySelector(".input-box input").value = ""
 			if (lvl >= 5) {
 				difficult++
-				console.log("difficult up")
 				lvl = 1
 				if (difficult == 6) {
-					alert("You win!")
-					lvl = 1
-					difficult = 1
-					wrongs = 0
+					document.querySelector(".win-box").style = "animation: win ease-out 1s forwards;"
+					setTimeout(() => {
+						lvl = 1
+						difficult = 1
+						wrongs = 0
+						document.querySelector(".win-box").style = "visibility: hidden; height: 0;"
+						updateData()
+						refresh()
+						return
+					}, 3000)
 				}
 			}
 		} else {
 			wrongs++
-			console.log("input != num", num, input_value)
-			console.log("wrong up")
 			document.querySelector(".input-box input").value = ""
 			if (wrongs == 3) {
 				document.querySelector(".wrongs").style = "animation: wrongg 1.5s ease-in;"
@@ -81,12 +80,12 @@ function process() {
 					document.querySelector(".wrongs").style = ""
 					updateData()
 					refresh()
+					return
 				}, 1500)
 			}
 		}
 		updateData()
 		refresh()
-		console.log("data updated")
 	} else {
 		console.log("ебать ошибка какая то")
 	}
@@ -134,7 +133,8 @@ function insertElements(block) {
 	let preinput_elem = document.createElement("span")
 	preinput_elem.textContent = "Number is: "
 	let input_elem = document.createElement("input")
-	input_elem.type = "text"
+	input_elem.type = "number"
+	input_elem.inputmode = "numeric"
 	input_elem.placeholder = "enter nothing to refresh"
 
 	input_box.insertAdjacentElement("beforeend", preinput_elem)
@@ -144,6 +144,17 @@ function insertElements(block) {
 	btn_elem.classList.add("btn")
 	btn_elem.textContent = "OK"
 
+	let win_box = document.createElement("div")
+	win_box.style = "visibility: hidden; height: 0;"
+	win_box.classList.add("win-box")
+
+	let congratulations = document.createElement("span")
+	congratulations.classList.add("cng")
+	congratulations.textContent = "You Win!"
+
+	win_box.insertAdjacentElement("beforeend", congratulations)
+
+	block.insertAdjacentElement("beforeend", win_box)
 	block.insertAdjacentElement("beforeend", data_box)
 	block.insertAdjacentElement("beforeend", num_elem)
 	block.insertAdjacentElement("beforeend", input_box)
@@ -160,7 +171,6 @@ updateData()
 
 document.querySelector(".btn").onclick = () => {
 	process()
-	console.log("button actived")
 }
 
 let accesskeys = [8,13,48,49,50,51,52,53,54,55,56,57,96,97,98,99,100,101,102,103,104,105]
